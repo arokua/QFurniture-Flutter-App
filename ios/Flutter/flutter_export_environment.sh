ad373ec526d04b7ee5cbc62b0762ca1f4d2ed57f
arokua <trannguyenphucanh@gmail.com>
2026-01-11 08:32:25 +1100

    fix config continue

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=E:\Program Files\flutter"
+export "FLUTTER_ROOT=E:\SOFT\flutter"
 export "FLUTTER_APPLICATION_PATH=G:\PA\flutter_ecommerce_template"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
