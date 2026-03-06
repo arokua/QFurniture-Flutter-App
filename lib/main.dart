4fa28c1580a18a2211038f76fe3fd65ff22a63bb
arokua <trannguyenphucanh@gmail.com>
2026-03-06 19:32:16 +1100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'app_router.dart';
+import 'features/catalog/data/product_remote_datasource.dart';
 import 'features/catalog/data/product_local_datasource.dart';
 import 'features/catalog/data/product_repository.dart';
+import 'config/store_cart_api_service.dart';
 
 final productRepoProvider = Provider<ProductRepository>(
-  (ref) => ProductRepository(ProductLocalDataSource()),
+  (ref) => ProductRepository(ProductLocalDataSource(), ProductRemoteDataSource()),
 );
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await StoreCartApiService.instance.init();
   runApp(const ProviderScope(child: AppRoot()));
 }
 
@@ -19,7 +23,10 @@ class AppRoot extends StatelessWidget {
     return MaterialApp.router(
       title: 'QFurniture',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(useMaterial3: true, colorSchemeSeed: const Color(0xFF4CAF50)),
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: const Color(0xFF795548), // Brown
+      ),
       routerConfig: router,
     );
   }
